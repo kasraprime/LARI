100cff7ad02fe0263891b09901e51fc6ff2b6d2b
unknown <phiggin1@umbc.edu>
2019-11-12 11:39:13 -0500

    added player that plays lowest card that can win trick, or lowest card in their hand otherwise

--- player_lari.py
+++ player_lari.py
@@ -1,6 +1,66 @@
 import random
 import operator
 
+def get_all_cards_suit(suit):
+    if suit == "D":
+        cards = {"2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD", "AD"}
+    elif suit == "S":
+        cards = {"2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS", "QS", "KS", "AS"}
+    elif suit == "H":
+        cards = {"2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH", "AH"}
+    elif suit == "C":
+        cards = {"2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC", "AC"}
+    else:
+        cards = set()
+
+    return cards
+
+def get_suit(card):
+    return card[1:]
+
+def get_rank(card):
+    rank = 0
+    r = card[:1]
+    if (r == "A"):
+        rank = 14
+    elif (r == "K"):
+        rank = 13
+    elif (r == "Q"):
+        rank = 12
+    elif (r == "J"):
+        rank = 11
+    elif (r == "T"):
+        rank == 10
+    else:
+        rank = int(r)
+    
+    return rank
+
+def get_min_card(cards):
+	min_val = get_rank(list(cards)[0])
+	min_card = list(cards)[0]
+	
+	for c in cards:
+		rank = get_rank(c)
+		if rank < min_val:
+			min_val = rank
+			min_card = c
+		
+	return min_card
+
+def get_max_card(cards):
+	max_val = get_rank(list(cards)[0])
+	max_card = list(cards)[0]
+	
+	for c in cards:
+		rank = get_rank(c)
+		if rank > max_val:
+			max_val = rank
+			max_card = c
+		
+	return max_card
+
+
 class Player(object):
     """
     Project Player interface.
@@ -167,4 +227,52 @@ class PlayerLARIJr(BasePlayer):
         super().collect_trick(lead, winner, trick)
         pass
 
+class PlayerPassiveLARI(BasePlayer):
 
+    def __init__(self):
+        super().__init__()
+        self.name = "1up L.A.R.I"
+        self._clear()
+
+    def get_name(self):
+        return self.name
+
+    def play_card(self, lead, trick):
+        #if we are leading the trick, play the lowest card in our hand
+        if lead == self.get_name():
+            card = get_min_card(self.hand)
+        else:
+            #get the suits for the trick
+            lead_suit = get_suit(trick[0])
+            #get all the vards in out hand that matches the suit of the trick
+            valid_cards = set(self.hand).intersection(get_all_cards_suit(lead_suit))
+            #if we do not have any cards of that suit play the lowest card in our hand
+            if len(valid_cards) == 0:
+                card = get_min_card(self.hand)
+            else:
+                #find the currently winng card in the trick
+                max_card_in_trick = get_max_card(set(trick).intersection(get_all_cards_suit(lead_suit)))
+                #find the highset card in our hand
+                max_card_in_hand = get_max_card(valid_cards)
+
+                #if we cannot win the trick play the lowest card of that suit in our hand
+                if(get_rank(max_card_in_trick) > get_rank(max_card_in_hand)):
+                    card = get_min_card(valid_cards)
+                else:
+                    #find the lowest card in our hand that wins the trick as it currently is
+                    for c in valid_cards:
+                        rank = get_rank(c)
+                        if rank < get_rank(max_card_in_hand) and rank > get_rank(max_card_in_trick):
+                            max_card_in_hand = c
+
+                    card = max_card_in_hand
+
+        #remove the card from our hand
+        self.hand.remove(card)
+
+        return card
+
+    def collect_trick(self, lead, winner, trick):
+        # TODO: Implement
+        super().collect_trick(lead, winner, trick)
+        pass
